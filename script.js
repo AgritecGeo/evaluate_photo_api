25b2b93bd42aaf1b39b6c106f71d92fe5b932c10
AgritecGeo <154455490+AgritecGeo@users.noreply.github.com>
2024-04-08 18:54:53 -0600

    new script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     cargarImagenesDesdeCSV();
 });
 
-// Función para cargar y mostrar imágenes desde datos CSV
 function cargarImagenesDesdeCSV() {
     fetch('tabla_documentacion.csv')
         .then(response => response.text())
@@ -13,7 +12,6 @@ function cargarImagenesDesdeCSV() {
         .catch(err => console.error('Error al cargar y parsear el CSV:', err));
 }
 
-// Función para parsear texto CSV y convertirlo a objetos de JavaScript
 function parseCSV(csvText) {
     const lines = csvText.trim().split('\n');
     const headers = lines.shift().split(',');
@@ -27,7 +25,6 @@ function parseCSV(csvText) {
     });
 }
 
-// Función para mostrar imágenes en la página
 function mostrarImagenes(data) {
     const imgContainer = document.getElementById('img-container');
     imgContainer.innerHTML = ''; // Limpia el contenedor antes de añadir nuevas imágenes
@@ -35,8 +32,13 @@ function mostrarImagenes(data) {
     data.forEach(imagen => {
         const imgDiv = document.createElement('div');
         imgDiv.classList.add('img-box');
+        // Asegúrate de ajustar la URL de la imagen según la ubicación y el nombre real de tus archivos de imagen
+        const imageURL = `https://filedn.com/lRAMUKU4tN3HUnQqI5npg4H/Plantix/Imagenes/imagen_${imagen.id}.png`;
         imgDiv.innerHTML = `
-            <img src="https://filedn.com/lRAMUKU4tN3HUnQqI5npg4H/Plantix/Imagenes/imagen_${imagen['ID_1707337378022']}.png" alt="${imagen['cultivo']}" class="image">
+            <div><strong>Nombre:</strong> ${imagen.id}</div>
+            <div><strong>País:</strong> ${imagen.pais}</div>
+            <div><strong>Cultivo:</strong> ${imagen.cultivo}</div>
+            <a href="${imageURL}" target="_blank"><img src="${imageURL}" alt="Imagen de ${imagen.cultivo}" class="image"></a>
             <textarea placeholder="Añade un comentario..."></textarea>
         `;
         imgContainer.appendChild(imgDiv);
